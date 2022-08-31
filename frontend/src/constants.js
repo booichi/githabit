b350faa42923d51b9fb164e255957b39eb863444
booichi <75922473+booichi@users.noreply.github.com>
2022-08-31 10:49:07 +0900

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
 export const GASAPI =
-  "https://script.google.com/macros/s/AKfycbxKf7yIntFUeC40lXlAz3jFUlSu96pp-swcrBc2c3tsG-XgRfFh3WfaB5cJ2ux8-7OCgQ/exec";
+  "https://script.google.com/macros/s/AKfycbzORJKO59OQHO2ta9bO91YKZ84NQwvn6AA_bIWAnYXMCnaT4SijHetbUZv_HyVWDf9DKw/exec";
